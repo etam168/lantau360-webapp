e40e3e978f17a08c7f5a194b6f7363276235184c
Shabeena <apps.shabeena@gmail.com>
2023-11-21 20:01:42 +0400

    WIP - Menu

---
+++ src/views/layout/footer/index.vue
@@ -0,0 +1,103 @@
+<template>
+  <q-footer bordered class="bg-grey-3 text-primary">
+    <q-btn-toggle
+      v-model="menuNav"
+      toggle-color="primary"
+      flat
+      size="16px"
+      text-color="grey-10"
+      style="width: 100%"
+      class="justify-center"
+      :options="[
+        { value: 'dashboard', slot: 'dashboard' },
+        { value: 'members', slot: 'members' },
+        { value: 'staff', slot: 'staff' },
+        { value: 'directories', slot: 'directories' },
+        { value: 'weather', slot: 'weather' }
+      ]"
+      @update:model-value="handleToggleUpdate"
+    >
+      <template v-slot:dashboard>
+        <q-item>
+          <q-item-section top class="text-center row items-center">
+            <q-item-label caption>Home</q-item-label>
+            <q-icon name="home" color="yellow" />
+          </q-item-section>
+        </q-item>
+      </template>
+
+      <template v-slot:members>
+        <q-item>
+          <q-item-section top class="text-center row items-center">
+            <q-item-label caption>Business</q-item-label>
+            <q-icon name="home" color="yellow" />
+          </q-item-section>
+        </q-item>
+      </template>
+
+      <template v-slot:staff>
+        <q-item>
+          <q-item-section top class="text-center row items-center">
+            <q-item-label caption>Community</q-item-label>
+            <q-icon name="home" color="yellow" />
+          </q-item-section>
+        </q-item>
+      </template>
+
+      <template v-slot:directories>
+        <q-item>
+          <q-item-section top class="text-center row items-center">
+            <q-item-label caption>Favourites</q-item-label>
+            <q-icon name="home" color="yellow" />
+          </q-item-section>
+        </q-item>
+      </template>
+
+      <template v-slot:weather>
+        <q-item>
+          <q-item-section top class="text-center row items-center">
+            <q-item-label caption>More</q-item-label>
+            <q-icon name="home" color="yellow" />
+          </q-item-section>
+        </q-item>
+      </template>
+    </q-btn-toggle>
+  </q-footer>
+</template>
+
+<script setup lang="ts">
+  // Vue Import
+  import { ref } from "vue";
+  import { useRouter } from "vue-router";
+
+  const router = useRouter();
+  const menuNav = ref("");
+
+  const handleToggleUpdate = (newVal: any) => {
+    switch (newVal) {
+      case "dashboard":
+        router.push("/dashboard");
+        break;
+      case "staff":
+        router.push("/staff");
+        break;
+      case "members":
+        router.push("/members");
+        break;
+      case "directories":
+        router.push("/directories");
+        break;
+      case "weather":
+        router.push("/weather");
+        break;
+      case "businesses":
+        router.push("/businesses");
+        break;
+      case "sites":
+        router.push("/sites");
+        break;
+      default:
+        break;
+    }
+  };
+</script>
--- src/views/layout/index.vue
+++ src/views/layout/index.vue
@@ -1,16 +1,24 @@
 <template>
-  <q-layout view="lHh lpr lFf" container style="height: 100vh" class="shadow-2 rounded-borders">
-    <header-component />
-    <q-page-container style="min-height: calc(100vh - 163px)">
-      <router-view :key="$route.fullPath"></router-view>
-    </q-page-container>
-    <footer-content />
-  </q-layout>
+  <q-card flat tile class="mx-auto mt-8 mb-2 pb-8 row justify-center">
+    <q-layout
+      view="lHh lpr lFf"
+      container
+      style="height: 100vh; max-width: 1024px"
+      class="shadow-2 rounded-borders"
+    >
+      <header-component />
+
+      <q-page-container style="min-height: calc(100vh - 163px)">
+        <router-view :key="$route.fullPath"></router-view>
+      </q-page-container>
+      <footer-component />
+    </q-layout>
+  </q-card>
 </template>
 
 <script setup lang="ts">
   // Vue Import
   //import { useUserStore } from "@/stores/user";
   import HeaderComponent from "./header/index.vue";
-  import FooterContent from "./footer-and-content/index.vue";
+  import FooterComponent from "./footer/index.vue";
 </script>
