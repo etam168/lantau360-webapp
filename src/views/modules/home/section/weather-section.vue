cdb4f11a93f68ef96ca566a437cab374cd97c93e
Shabeena <apps.shabeena@gmail.com>
2023-11-22 14:20:39 +0400

    Date Formatted in Weather Section.

--- src/views/modules/home/section/weather-section.vue
+++ src/views/modules/home/section/weather-section.vue
@@ -8,7 +8,7 @@
         <q-item-section top>
           <i class="fa-regular fa-solid fa-cloud text-h2"></i>
           <q-item-label class="text-weight-medium text-h6">{{ data?.caption }}</q-item-label>
-          <q-item-label class="text-subtitle1">{{ data?.forecastDateTime }}</q-item-label>
+          <q-item-label class="text-subtitle1">{{ weatherDate }}</q-item-label>
         </q-item-section>
 
         <q-item-section top side>
@@ -32,15 +32,19 @@
 </template>
 <script setup lang="ts">
   // Vue Import
-  import { PropType } from "vue";
-
+  import { PropType, computed } from "vue";
+  import { date } from "quasar";
   // .ts file
   import { Weather } from "@/interfaces/models/entities/weather";
-  defineProps({
+  const props = defineProps({
     data: {
       type: Object as PropType<Weather | null>,
       required: false,
       default: null
     }
   });
+
+  const weatherDate = computed(() =>
+    date.formatDate(props.data?.forecastDateTime, "YYYY-MM-DD HH:MM")
+  );
 </script>
