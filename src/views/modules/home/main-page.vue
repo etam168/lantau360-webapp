da491623706a24ed78f5c844eba9600a9144b5eb
Shabeena <apps.shabeena@gmail.com>
2023-11-23 14:32:14 +0400

    Home Module - Fix the Directory File Path.

--- src/views/modules/home/main-page.vue
+++ src/views/modules/home/main-page.vue
@@ -20,7 +20,7 @@
   // Custom Components
   import HeroSection from "./section/hero-section.vue";
   import weatherSection from "./section/weather-section.vue";
-  import DirectoryList from "./section/directory/directory-list.vue";
+  import DirectoryList from "../shared/directory/directory-list.vue";
   import { Weather } from "@/interfaces/models/entities/weather";
   import { Directory } from "@/interfaces/models/entities/directory";
 
