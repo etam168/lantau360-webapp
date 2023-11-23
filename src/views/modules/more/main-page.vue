4702840d20650cf7026f969ce193e2e6a5a7250b
Shabeena <apps.shabeena@gmail.com>
2023-11-23 17:46:04 +0400

    More Module - Bottom Sheet show on 4 Items (Language Settings, About Us, Terms & Conditions and Privacy Policy).

--- src/views/modules/more/main-page.vue
+++ src/views/modules/more/main-page.vue
@@ -26,23 +26,64 @@
   import { useI18n } from "vue-i18n";
   import data from "./data/data.json";
   import { MoreItem } from "@/interfaces/models/entities/moreItem";
+  import i18n from "@/plugins/i18n/i18n";
 
   const moreItems = ref<MoreItem[]>([]);
   const $q = useQuasar();
+  const { t } = i18n.global;
 
   const { locale } = useI18n({ useScope: "global" });
 
+  const shouldShowBottomSheet = (item: MoreItem) => {
+    return ["language_settings", "about_us", "tnc", "privacy_policy"].includes(item.ResKey);
+  };
+
   const showBottomSheet = (item: MoreItem) => {
-    if (item.ResKey === "language_settings") {
+    if (shouldShowBottomSheet(item)) {
+      let actions;
+      let message = "";
+
+      switch (item.ResKey) {
+        case "language_settings":
+          actions = [
+            { label: "En", value: "en", onClick: () => changeLanguage("en") },
+            { label: "繁", value: "hk", onClick: () => changeLanguage("hk") },
+            { label: "简", value: "cn", onClick: () => changeLanguage("cn") }
+          ];
+          message = t("more.language");
+          break;
+        case "about_us":
+          // Customize actions for about us
+          actions = [
+            // Add actions for about us
+          ];
+          message = t("more.aboutUs");
+
+          break;
+        case "tnc":
+          // Customize actions for terms and conditions
+          actions = [
+            // Add actions for terms and conditions
+          ];
+          message = t("more.termsConditions");
+
+          break;
+        case "privacy_policy":
+          // Customize actions for privacy policy
+          actions = [
+            // Add actions for privacy policy
+          ];
+          message = t("more.privacyPolicy");
+          break;
+        default:
+          actions = [];
+      }
+
       $q.bottomSheet({
         dark: true,
-        message: "Bottom Sheet message",
+        message,
         grid: true,
-        actions: [
-          { label: " En", value: "en", onClick: () => changeLanguage("en") },
-          { label: "繁", value: "hk", onClick: () => changeLanguage("hk") },
-          { label: "简", value: "cn", onClick: () => changeLanguage("cn") }
-        ]
+        actions
       })
         .onOk(action => {
           console.log("Action chosen:", action.value);
