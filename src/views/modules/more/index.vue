8dfb8b92068ebf98b1efb815038ddaa9a8767bb5
Shabeena <apps.shabeena@gmail.com>
2023-12-13 11:52:26 +0400

    More Module UI fixed.

--- src/views/modules/more/index.vue
+++ src/views/modules/more/index.vue
@@ -1,54 +1,52 @@
 <template>
   <q-page>
     <app-page-title :title="$t('more.title')"></app-page-title>
-    <q-card-section horizontal class="justify-between" v-if="$q.screen.gt.sm">
-      <q-item v-if="userStore.token">
-        <q-item-section>
-          <q-item-label>{{ userStore.user }}</q-item-label>
-          <q-item-label caption> {{ $t("more.loginName") }} </q-item-label>
+    <q-card-section class="q-pb-none">
+      <q-item class="shadow-1 q-mb-md q-pl-sm">
+        <q-item-section top avatar>
+          <q-btn outline round color="black" class="q-mx-auto">
+            <q-avatar size="76px">
+              <q-img :src="computePath">
+                <template v-slot:error>
+                  <q-img :src="PLACEHOLDER_AVATAR" />
+                </template>
+
+                <template v-slot:loading>
+                  <div class="absolute-full flex flex-center bg-gray text-white">
+                    <q-inner-loading showing class="spinner-card row justify-center items-center">
+                      <q-spinner size="50px" color="primary" />
+                    </q-inner-loading>
+                  </div>
+                </template>
+              </q-img>
+
+              <q-badge class="absolute-bottom-left" color="transparent" v-if="userStore.token">
+                <app-button
+                  round
+                  color="black"
+                  icon="photo_camera"
+                  size="xs"
+                  @click="onImageUpload"
+                />
+              </q-badge>
+            </q-avatar>
+
+            <q-file
+              ref="imageRef"
+              v-show="false"
+              v-model="imagePath"
+              @update:model-value="uploadImage"
+            >
+            </q-file>
+          </q-btn>
         </q-item-section>
-      </q-item>
 
-      <q-item>
-        <q-btn outline round color="black" v-if="userStore.token" class="q-mx-auto">
-          <q-avatar size="120px">
-            <q-img :src="computePath">
-              <template v-slot:error>
-                <q-img :src="PLACEHOLDER_AVATAR" />
-              </template>
-
-              <template v-slot:loading>
-                <div class="absolute-full flex flex-center bg-gray text-white">
-                  <q-inner-loading showing class="spinner-card row justify-center items-center">
-                    <q-spinner size="50px" color="primary" />
-                  </q-inner-loading>
-                </div>
-              </template>
-            </q-img>
-
-            <q-badge class="absolute-bottom-left" color="transparent">
-              <app-button
-                round
-                color="black"
-                icon="photo_camera"
-                size="xs"
-                @click="onImageUpload"
-              />
-            </q-badge>
-          </q-avatar>
-
-          <q-file
-            ref="imageRef"
-            v-show="false"
-            v-model="imagePath"
-            @update:model-value="uploadImage"
-          >
-          </q-file>
-        </q-btn>
-      </q-item>
-
-      <q-item>
         <q-item-section>
+          <q-item-label v-if="userStore.token">{{ userStore.user }}</q-item-label>
+          <q-item-label caption v-if="userStore.token"> {{ $t("more.loginName") }} </q-item-label>
+        </q-item-section>
+
+        <q-item-section side>
           <div class="text-grey-8 q-gutter-xs">
             <q-chip
               v-if="userStore.token"
@@ -84,89 +82,7 @@
       </q-item>
     </q-card-section>
 
-    <q-card-section class="justify-between q-pb-none" v-else>
-      <q-item class="q-pa-none">
-        <q-item-section>
-          <q-item-section v-if="userStore.token">
-            <q-item-label>{{ userStore.user }}</q-item-label>
-            <q-item-label caption> {{ $t("more.loginName") }} </q-item-label>
-          </q-item-section>
-          <q-item-label>
-            <q-item-section>
-              <div class="text-grey-8 q-gutter-xs">
-                <q-chip
-                  v-if="userStore.token"
-                  clickable
-                  @click="logout()"
-                  outline
-                  color="primary"
-                  text-color="white"
-                >
-                  {{ $t("auth.login.logout") }}
-                </q-chip>
-                <q-chip
-                  v-if="!userStore.token"
-                  clickable
-                  @click="showLoginDialog('login')"
-                  outline
-                  color="primary"
-                  text-color="white"
-                >
-                  {{ $t("auth.login.button") }}
-                </q-chip>
-                <q-chip
-                  v-if="!userStore.token"
-                  clickable
-                  @click="showLoginDialog('register')"
-                  color="primary"
-                  text-color="white"
-                >
-                  {{ $t("auth.register.joinNow") }}
-                </q-chip>
-              </div>
-            </q-item-section></q-item-label
-          >
-        </q-item-section>
-        <q-item-section side
-          ><q-btn outline round color="black" v-if="userStore.token" class="q-mx-auto">
-            <q-avatar size="80px">
-              <q-img :src="computePath">
-                <template v-slot:error>
-                  <q-img :src="PLACEHOLDER_AVATAR" />
-                </template>
-
-                <template v-slot:loading>
-                  <div class="absolute-full flex flex-center bg-gray text-white">
-                    <q-inner-loading showing class="spinner-card row justify-center items-center">
-                      <q-spinner size="50px" color="primary" />
-                    </q-inner-loading>
-                  </div>
-                </template>
-              </q-img>
-
-              <q-badge class="absolute-bottom-left" color="transparent">
-                <app-button
-                  round
-                  color="black"
-                  icon="photo_camera"
-                  size="xs"
-                  @click="onImageUpload"
-                />
-              </q-badge>
-            </q-avatar>
-
-            <q-file
-              ref="imageRef"
-              v-show="false"
-              v-model="imagePath"
-              @update:model-value="uploadImage"
-            >
-            </q-file> </q-btn
-        ></q-item-section>
-      </q-item>
-    </q-card-section>
-
-    <q-card-section>
+    <q-card-section class="q-pt-none">
       <q-item
         v-for="item in menuItems"
         :key="item.resKey"
