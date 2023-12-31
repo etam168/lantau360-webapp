cbdc13d7ef69d29c2ad1dbf12cbcdd9c76075969
Suleman Khalid <sulemankhalid218@gmail.com>
2023-12-31 15:21:26 +0500

    Tidy up profile setting dialog

--- src/views/modules/more/section/profile-setting-dialog.vue
+++ src/views/modules/more/section/profile-setting-dialog.vue
@@ -1,119 +1,98 @@
 <template>
-  <suspense>
-    <template #default>
-      <q-dialog
-        ref="dialogRef"
-        @hide="onDialogHide"
-        transition-show="slide-up"
-        transition-hide="slide-down"
-        @update:model-value="updateDialogState"
-        :model-value="isDialogVisible"
-        maximized
-      >
-        <q-layout view="lHh lpr lFf" class="bg-white" style="max-width: 1024px">
-          <q-header class="bg-transparent text-dark">
-            <app-dialog-title>{{ $t(`more.profileSetting.title`) }}</app-dialog-title>
-          </q-header>
+  <q-dialog
+    ref="dialogRef"
+    @hide="onDialogHide"
+    transition-show="slide-up"
+    transition-hide="slide-down"
+    @update:model-value="updateDialogState"
+    :model-value="isDialogVisible"
+    maximized
+  >
+    <q-layout view="lHh lpr lFf" class="bg-white" style="max-width: 1024px">
+      <q-header class="bg-transparent text-dark">
+        <app-dialog-title>{{ $t(`more.profileSetting.title`) }}</app-dialog-title>
+      </q-header>
 
-          <q-page-container>
-            <q-page>
-              <q-card flat>
-                <q-card-actions>
-                  <q-btn outline round color="black" class="q-mr-xl">
-                    <q-avatar size="128px">
-                      <q-img :src="computePath">
-                        <template v-slot:error>
-                          <q-img :src="PLACEHOLDER_AVATAR" />
-                        </template>
+      <q-page-container>
+        <q-page>
+          <q-card flat>
+            <q-card-actions>
+              <q-btn outline round color="black" class="q-mr-xl">
+                <q-avatar size="128px">
+                  <q-img :src="computePath">
+                    <template v-slot:error>
+                      <q-img :src="PLACEHOLDER_AVATAR" />
+                    </template>
 
-                        <template v-slot:loading>
-                          <div class="absolute-full flex flex-center bg-gray text-white">
-                            <q-inner-loading
-                              showing
-                              class="spinner-card row justify-center items-center"
-                            >
-                              <q-spinner size="50px" color="primary" />
-                            </q-inner-loading>
-                          </div>
-                        </template>
-                      </q-img>
+                    <template v-slot:loading>
+                      <div class="absolute-full flex flex-center bg-gray text-white">
+                        <q-inner-loading
+                          showing
+                          class="spinner-card row justify-center items-center"
+                        >
+                          <q-spinner size="50px" color="primary" />
+                        </q-inner-loading>
+                      </div>
+                    </template>
+                  </q-img>
 
-                      <q-badge class="absolute-bottom-left" color="transparent">
-                        <app-button
-                          round
-                          color="black"
-                          icon="photo_camera"
-                          size="xs"
-                          @click="onImageUpload"
-                        />
-                      </q-badge>
-                    </q-avatar>
-
-                    <q-file
-                      ref="imageRef"
-                      v-show="false"
-                      v-model="imagePath"
-                      @update:model-value="uploadImage"
-                    >
-                    </q-file>
-                  </q-btn>
-                  <div class="text-subtitle1 text-weight-medium">
-                    {{ $t("more.profileSetting.subtitle") }}
-                  </div>
-                </q-card-actions>
-
-                <q-card-section>
-                  <Form
-                    ref="form"
-                    class="full-height"
-                    :initial-values="initialValues"
-                    :validation-schema="schema"
-                    @submit="onSubmit"
-                    v-slot="{ meta }"
-                  >
-                    <vee-input
-                      :label="$t('auth.register.email')"
-                      icon="mdi-account"
-                      name="email"
-                      disable
-                      placeholder="user@example.com"
-                    />
-                    <vee-input
-                      :label="$t('auth.register.firstName')"
-                      icon="mdi-account"
-                      name="firstName"
-                      placeholder="First Name"
-                    />
-                    <vee-input
-                      :label="$t('auth.register.lastName')"
-                      icon="mdi-account"
-                      name="lastName"
-                      placeholder="Last Name"
-                    />
-
-                    <vee-input
-                      :label="$t('auth.register.phone')"
-                      icon="mdi-account"
-                      name="phone"
-                      placeholder="Phone"
-                    />
+                  <q-badge class="absolute-bottom-left" color="transparent">
                     <app-button
-                      class="full-width"
-                      :label="$t('more.profileSetting.save')"
-                      type="submit"
+                      round
+                      color="black"
+                      icon="photo_camera"
+                      size="xs"
+                      @click="onImageUpload"
                     />
-                  </Form>
-                </q-card-section>
-              </q-card>
-            </q-page>
-          </q-page-container>
-        </q-layout>
-      </q-dialog>
-    </template>
-    <template #fallback>
-      <div class="row justify-center items-center" style="height: 500px"></div>
-    </template>
-  </suspense>
+                  </q-badge>
+                </q-avatar>
+
+                <q-file
+                  ref="imageRef"
+                  v-show="false"
+                  v-model="imagePath"
+                  @update:model-value="uploadImage"
+                >
+                </q-file>
+              </q-btn>
+              <div class="text-subtitle1 text-weight-medium">
+                {{ $t("more.profileSetting.subtitle") }}
+              </div>
+            </q-card-actions>
+
+            <q-card-section>
+              <Form
+                ref="form"
+                class="full-height"
+                :initial-values="initialValues"
+                :validation-schema="schema"
+                @submit="onSubmit"
+                v-slot="{ meta }"
+              >
+                <q-item v-for="(field, index) in fields" :key="index">
+                  <q-item-section>
+                    <vee-input
+                      :name="field.name"
+                      :maxlength="field.maxlength"
+                      :disable="field.disable"
+                      stack-label
+                      icon="mdi-account"
+                      :label="$t(field.label)"
+                    />
+                  </q-item-section>
+                </q-item>
+                <app-button
+                  class="full-width"
+                  :label="$t('more.profileSetting.save')"
+                  type="submit"
+                />
+              </Form>
+            </q-card-section>
+          </q-card>
+        </q-page>
+      </q-page-container>
+    </q-layout>
+  </q-dialog>
 </template>
 
 <script setup lang="ts">
@@ -194,4 +173,25 @@
     isDialogVisible.value = status;
     eventBus.emit("DialogStatus", status, "ProfileSettingDialog");
   }
+
+  const fields = [
+    {
+      name: "email",
+      maxlength: 20,
+      label: "auth.register.email",
+      disable: true
+    },
+    {
+      name: "firstName",
+      label: "auth.register.firstName"
+    },
+    {
+      name: "lastName",
+      label: "auth.register.lastName"
+    },
+    {
+      name: "phone",
+      label: "auth.register.phone"
+    }
+  ];
 </script>
