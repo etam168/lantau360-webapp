a5c7e9e02ca8b6779285fcf0e0340d136c8765a4
Syed-Nabi <syednabi.uom@gmai.com>
2024-01-16 12:03:07 +0500

    WIP: Posting input dialog UI

--- src/views/modules/community/shared/input-content/index.vue
+++ src/views/modules/community/shared/input-content/index.vue
@@ -1,23 +1,139 @@
 <template>
   <q-list dense>
     <q-item class="q-mb-sm">
-      <toggle-status name="status" dense class="q-pb-none" />
+      <toggle-status name="status" />
     </q-item>
 
-    <template v-for="field in veeInputsForPosting" :key="field">
-      <q-item>
-        <q-item-section>
-          <vee-input
-            :name="field.name"
-            :auto-grow="true"
-            :maxlength="field.maxlength"
-            stack-label
-            :label="$t(field.label)"
-            :input-type="field.type"
-          />
-        </q-item-section>
-      </q-item>
-    </template>
+    <q-item>
+      <q-item-section>
+        <q-card flat>
+          <div class="q-py-md">
+            <q-list bordered class="rounded-borders">
+              <q-expansion-item group="siteGroup" dense dense-toggle default-opened>
+                <template v-slot:header>
+                  <q-item-section avatar>
+                    <q-avatar size="24px" color="primary" text-color="white">en</q-avatar>
+                  </q-item-section>
+
+                  <q-item-section> English </q-item-section>
+                </template>
+                <q-card>
+                  <q-card-section>
+                    <vee-input
+                      name="postingName"
+                      :label="$t('posting.columns.postingName')"
+                      :maxlength="32"
+                      stack-label
+                    />
+                    <vee-input
+                      name="title"
+                      :label="$t('posting.columns.title')"
+                      :maxlength="25"
+                      counter
+                      stack-label
+                    />
+
+                    <template v-for="field in veeInputsForEnSubtitle" :key="field">
+                      <vee-input
+                        :name="field.name"
+                        :auto-grow="true"
+                        :maxlength="field.maxlength"
+                        stack-label
+                        :label="$t(field.label)"
+                      />
+                    </template>
+                  </q-card-section>
+                </q-card>
+              </q-expansion-item>
+
+              <q-expansion-item group="siteGroup" dense dense-toggle>
+                <template v-slot:header>
+                  <q-item-section avatar>
+                    <q-avatar size="24px" color="primary" text-color="white">hk</q-avatar>
+                  </q-item-section>
+
+                  <q-item-section> 繁體版本 </q-item-section>
+                </template>
+                <q-card>
+                  <q-card-section>
+                    <vee-input
+                      name="postingNameHk"
+                      :label="$t('posting.columns.postingNameHk')"
+                      :maxlength="50"
+                      stack-label
+                    />
+                    <vee-input
+                      name="titleHk"
+                      :label="$t('posting.columns.titleHk')"
+                      :maxlength="50"
+                      counter
+                      stack-label
+                    />
+                    <template v-for="field in veeInputsForHkSubtitle" :key="field">
+                      <vee-input
+                        :name="field.name"
+                        :auto-grow="true"
+                        :maxlength="field.maxlength"
+                        stack-label
+                        :label="$t(field.label)"
+                      />
+                    </template>
+                  </q-card-section>
+                </q-card>
+              </q-expansion-item>
+
+              <q-expansion-item group="siteGroup" dense dense-toggle>
+                <template v-slot:header>
+                  <q-item-section avatar>
+                    <q-avatar size="24px" color="primary" text-color="white">cn</q-avatar>
+                  </q-item-section>
+
+                  <q-item-section> 简体版本 </q-item-section>
+                </template>
+                <q-card>
+                  <q-card-section>
+                    <vee-input
+                      name="postingNameCn"
+                      :label="$t('posting.columns.postingNameCn')"
+                      :maxlength="50"
+                      stack-label
+                    />
+                    <vee-input
+                      name="titleCn"
+                      :label="$t('posting.columns.titleCn')"
+                      :maxlength="50"
+                      counter
+                      stack-label
+                    />
+                    <template v-for="field in veeInputsForCnSubtitle" :key="field">
+                      <vee-input
+                        :name="field.name"
+                        :auto-grow="true"
+                        :maxlength="field.maxlength"
+                        stack-label
+                        :label="$t(field.label)"
+                      />
+                    </template>
+                  </q-card-section>
+                </q-card>
+              </q-expansion-item>
+            </q-list>
+          </div>
+        </q-card>
+      </q-item-section>
+    </q-item>
+
+    <q-item>
+      <q-item-section>
+        <vee-input name="rank" :auto-grow="true" :label="$t('posting.columns.rank')" />
+      </q-item-section>
+    </q-item>
+
+    <q-item>
+      <q-item-section v-for="field in veeInputsForLatLng" :key="field.name">
+        <vee-input :name="field.name" :auto-grow="true" :label="$t(field.label)" />
+      </q-item-section>
+    </q-item>
 
     <!-- Contact Info -->
 
@@ -34,52 +150,43 @@
           />
         </q-item-section>
       </q-item>
-      <template v-for="contact in veeInputsForContactInfo" :key="contact">
-        <q-item
-          ><q-item-section>
-            <vee-tel-input
-              class="q-mb-sm"
+      <template v-for="contact in veeInputsForContactInfo" :key="contact.name">
+        <q-item>
+          <q-item-section>
+            <vee-q-tel-input
               :name="contact.name"
-              :default-country="contact.defaultIso"
-              :labelText="$t(contact.label)" /></q-item-section
-        ></q-item>
-
-        <template v-for="contact in veeInputsForContactWhatsapp" :key="contact">
-          <q-item>
-            <q-item-section>
-              <vee-input-whatsapp
-                :name="contact.name"
-                stack-label
-                :label="$t(contact.label)"
-                :max-lenght="contact.maxLenght"
-                :counter="true"
-              />
-            </q-item-section>
-          </q-item>
-        </template>
+              :defaultIso="contact.defaultIso"
+              :labelText="$t(contact.label)"
+              :label="$t(contact.label)"
+          /></q-item-section>
+        </q-item>
       </template>
     </q-list>
   </q-list>
 </template>
 
 <script setup lang="ts">
-  import { ref } from "vue";
-
-  const veeInputsForPosting = ref([
-    { name: "postingName", label: "posting.columns.postingName", maxlength: 32 },
-    { name: "title", label: "posting.columns.title", maxlength: 25 },
+  const veeInputsForEnSubtitle = ref([
     { name: "subtitle1", label: "posting.columns.subtitle1", maxlength: 50 },
     { name: "subtitle2", label: "posting.columns.subtitle2", maxlength: 50 },
-    { name: "subtitle3", label: "posting.columns.subtitle3", maxlength: 50 },
-    { name: "latitude", label: "posting.columns.latitude", type: "number" },
-    { name: "longitude", label: "posting.columns.longitude", type: "number" },
-    { name: "postingNameHk", label: "posting.columns.postingNameHk", maxlength: 50 },
-    { name: "postingNameCn", label: "posting.columns.postingNameCn", maxlength: 50 },
-    { name: "titleHk", label: "posting.columns.titleHk", maxlength: 50 },
-    { name: "titleCn", label: "posting.columns.titleCn", maxlength: 50 },
+    { name: "subtitle3", label: "posting.columns.subtitle3", maxlength: 50 }
+  ]);
+
+  const veeInputsForHkSubtitle = ref([
     { name: "subtitle1Hk", label: "posting.columns.subtitle1Hk", maxlength: 50 },
+    { name: "subtitle2Hk", label: "posting.columns.subtitle2Hk", maxlength: 50 },
+    { name: "subtitle3Hk", label: "posting.columns.subtitle3Hk", maxlength: 50 }
+  ]);
+
+  const veeInputsForCnSubtitle = ref([
     { name: "subtitle1Cn", label: "posting.columns.subtitle1Cn", maxlength: 50 },
-    { name: "rank", label: "posting.columns.rank", type: "number" }
+    { name: "subtitle2Cn", label: "posting.columns.subtitle2Cn", maxlength: 50 },
+    { name: "subtitle3Cn", label: "posting.columns.subtitle3Cn", maxlength: 50 }
+  ]);
+
+  const veeInputsForLatLng = ref([
+    { name: "latitude", label: "posting.columns.latitude", type: "number" },
+    { name: "longitude", label: "posting.columns.longitude", type: "number" }
   ]);
 
   const veeInputsForContactOther = ref({
@@ -94,16 +201,13 @@
       name: "contactPhone",
       label: "posting.columns.contactPhone",
       icon: "phone",
-      type: "tel",
-      defaultIso: "HK"
-    }
-  ]);
-
-  const veeInputsForContactWhatsapp = ref([
+      defaultIso: "hk"
+    },
     {
       name: "contactWhatsApp",
       label: "posting.columns.contactWhatsApp",
-      maxLenght: 8
+      icon: "phone",
+      defaultIso: "hk"
     }
   ]);
 </script>
