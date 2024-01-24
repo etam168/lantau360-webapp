e908005a9a3b3bf8d08ca00b736daff3d84a8db8
Syed-Nabi <syednabi.uom@gmai.com>
2024-01-24 11:09:01 +0500

    Fix site detail action buttons

--- src/components/dialog/renderer/site-renderer.vue
+++ src/components/dialog/renderer/site-renderer.vue
@@ -8,6 +8,20 @@
       </q-item-section>
 
       <q-item-section side>
+        <app-button-rounded
+          :text-color="isFavourite ? 'red' : 'white'"
+          icon="favorite"
+          :class="{ 'margin-top-right': imageCount > 1, 'margin-top': imageCount <= 1 }"
+          @click="onBtnFavClick"
+        />
+      </q-item-section>
+    </q-item>
+
+    <q-item>
+      <app-text-editor v-model="translatedContent" />
+    </q-item>
+    <q-item>
+      <q-item-section side>
         <div class="q-gutter-md">
           <app-button-rounded v-if="item.contactPhone" icon="phone" @click="navigateToPhone" />
 
@@ -16,18 +30,8 @@
             icon="fab fa-whatsapp"
             @click="navigateToWhatsApp(item.contactWhatsApp)"
           />
-          <app-button-rounded
-            :text-color="isFavourite ? 'red' : 'white'"
-            icon="favorite"
-            :class="{ 'margin-200': imageCount > 1, 'margin-40': imageCount <= 1 }"
-            @click="onBtnFavClick"
-          />
         </div>
       </q-item-section>
-    </q-item>
-
-    <q-item>
-      <app-text-editor v-model="translatedContent" />
     </q-item>
   </q-list>
 </template>
@@ -95,11 +99,12 @@
   };
 </script>
 <style scoped>
-  .margin-200 {
-    margin-top: -200px;
+  .margin-top {
+    margin-top: -50px;
   }
 
-  .margin-40 {
-    margin-top: -40px;
+  .margin-top-right {
+    margin-top: -50px;
+    margin-right: -15px;
   }
 </style>
