4cfe7a12b3d68484586aa256ab5713f94400c681
Syed-Nabi <syednabi.uom@gmai.com>
2024-05-21 12:50:02 +0500

    Sort image-icon fields alphabitical

--- src/constants/app/image-icon.ts
+++ src/constants/app/image-icon.ts
@@ -1,22 +1,22 @@
 export const ICONS = {
-  SETTING: "./resources/icons/ic_language_setting.svg",
-  TNC: "./resources/icons/ic_terms_conditions.svg",
+  ACCOUNT: "./resources/icons/ic_privacy.svg",
   PRIVACY: "./resources/icons/ic_privacy.svg",
   PROFILE: "./resources/icons/ic_privacy.svg",
-  ACCOUNT: "./resources/icons/ic_privacy.svg"
+  SETTING: "./resources/icons/ic_language_setting.svg",
+  TNC: "./resources/icons/ic_terms_conditions.svg"
 };
 
 export const IMAGES = {
-  NO_IMAGE_AVAILABLE_PLACEHOLDER: "./resources/images/no_image_available_placeholder.jpeg",
-  NO_DATA_IMAGE: "./resources/images/no_data_image.png",
-  CREDIT_CARD_FRONT: "./resources/images/visa_card_front.png",
-  CREDIT_CARD_BACK: "./resources/images/visa_card_back.png",
-  TRANACTION_SUCCESS: "./resources/images/transaction_success.jpeg",
-  TRANSACTION_CANCELLED: "./resources/images/transaction_cancelled.png",
-  INSUFFICIENT_POINT: "./resources/images/insufficient.png",
+  CAROUSEL_BACKGROUND: "./resources/images/home-bg.webp",
   CHECK_IN: "./resources/images/check_in.jpg",
+  CREDIT_CARD_BACK: "./resources/images/visa_card_back.png",
+  CREDIT_CARD_FRONT: "./resources/images/visa_card_front.png",
+  INSUFFICIENT_POINT: "./resources/images/insufficient.png",
   LOGO: "./resources/logo/logo.png",
-  CAROUSEL_BACKGROUND: "@/assets/resources/home-bg.webp",
+  NO_DATA_IMAGE: "./resources/images/no_data_image.png",
+  NO_IMAGE_AVAILABLE_PLACEHOLDER: "./resources/images/no_image_available_placeholder.jpeg",
+  NO_INTERNET: "./resources/images/no_wifi.png",
   PLACEHOLDER_AVATAR: "./resources/images/avatar_placeholder.png",
-  NO_INTERNET: "./resources/images/no_wifi.png"
+  TRANACTION_SUCCESS: "./resources/images/transaction_success.jpeg",
+  TRANSACTION_CANCELLED: "./resources/images/transaction_cancelled.png"
 };
