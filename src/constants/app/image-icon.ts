2494c78059e1c8ee25857a34cfb9ac371e4c05c9
Syed-Nabi <syednabi.uom@gmai.com>
2024-05-21 12:30:56 +0500

    Rename public img directory to resouces

--- src/constants/app/image-icon.ts
+++ src/constants/app/image-icon.ts
@@ -1,22 +1,22 @@
 export const ICONS = {
-  SETTING: "./img/icons/ic_language_setting.svg",
-  TNC: "./img/icons/ic_terms_conditions.svg",
-  PRIVACY: "./img/icons/ic_privacy.svg",
-  PROFILE: "./img/icons/ic_privacy.svg",
-  ACCOUNT: "./img/icons/ic_privacy.svg"
+  SETTING: "./resources/icons/ic_language_setting.svg",
+  TNC: "./resources/icons/ic_terms_conditions.svg",
+  PRIVACY: "./resources/icons/ic_privacy.svg",
+  PROFILE: "./resources/icons/ic_privacy.svg",
+  ACCOUNT: "./resources/icons/ic_privacy.svg"
 };
 
 export const IMAGES = {
-  NO_IMAGE_AVAILABLE_PLACEHOLDER: "./img/images/no_image_available_placeholder.jpeg",
-  NO_DATA_IMAGE: "./img/images/no_data_image.png",
-  CREDIT_CARD_FRONT: "./img/images/visa_card_front.png",
-  CREDIT_CARD_BACK: "./img/images/visa_card_back.png",
-  TRANACTION_SUCCESS: "./img/images/transaction_success.jpeg",
-  TRANSACTION_CANCELLED: "./img/images/transaction_cancelled.png",
-  INSUFFICIENT_POINT: "./img/images/insufficient.png",
-  CHECK_IN: "./img/images/check_in.jpg",
-  LOGO: "./img/logo/logo.png",
-  CAROUSEL_BACKGROUND: "@/assets/img/home-bg.webp",
-  PLACEHOLDER_AVATAR: "./img/images/avatar_placeholder.png",
-  NO_INTERNET: "./img/images/no_wifi.png"
+  NO_IMAGE_AVAILABLE_PLACEHOLDER: "./resources/images/no_image_available_placeholder.jpeg",
+  NO_DATA_IMAGE: "./resources/images/no_data_image.png",
+  CREDIT_CARD_FRONT: "./resources/images/visa_card_front.png",
+  CREDIT_CARD_BACK: "./resources/images/visa_card_back.png",
+  TRANACTION_SUCCESS: "./resources/images/transaction_success.jpeg",
+  TRANSACTION_CANCELLED: "./resources/images/transaction_cancelled.png",
+  INSUFFICIENT_POINT: "./resources/images/insufficient.png",
+  CHECK_IN: "./resources/images/check_in.jpg",
+  LOGO: "./resources/logo/logo.png",
+  CAROUSEL_BACKGROUND: "@/assets/resources/home-bg.webp",
+  PLACEHOLDER_AVATAR: "./resources/images/avatar_placeholder.png",
+  NO_INTERNET: "./resources/images/no_wifi.png"
 };
