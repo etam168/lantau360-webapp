03f734a7b326390a96d6c7f74f0d2d6fe78cc25c
Syed-Nabi <syednabi.uom@gmai.com>
2024-05-25 11:29:51 +0500

    Update index page

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,89 @@
 <template>
   <router-view />
   <!-- <AddToHomeScreen /> -->
-  <div v-show="showInstallPrompt" class="install-banner" @click="showInstallationGuide">
+  <div v-show="showIosInstallPrompt" class="install-banner" @click="showIosInstallationGuide">
+    {{ $t("installApp.title") }}
+  </div>
+  <div v-show="showAndroidInstallPrompt" class="install-banner" @click="promptAndroidInstall">
     {{ $t("installApp.title") }}
   </div>
 </template>
 
 <script setup lang="ts">
+  import { ref, onMounted } from "vue";
+
   const isIos = () => {
     const userAgent = window.navigator.userAgent.toLowerCase();
     return /iphone|ipad|ipod/.test(userAgent);
   };
 
-  const isInStandaloneMode = () => "standalone" in window.navigator && window.navigator.standalone;
+  const isAndroid = () => {
+    const userAgent = window.navigator.userAgent.toLowerCase();
+    return /android/.test(userAgent);
+  };
 
-  const showInstallPrompt = ref(isIos() && !isInStandaloneMode());
+  const isInStandaloneMode = () => {
+    return (
+      window.matchMedia("(display-mode: standalone)").matches ||
+      // For older iOS versions
+      ("standalone" in window.navigator && window.navigator.standalone)
+    );
+  };
 
-  const showInstallationGuide = () => {
+  const showIosInstallPrompt = ref(false);
+  const showAndroidInstallPrompt = ref(false);
+  let deferredPrompt: any = null;
+
+  const showIosInstallationGuide = () => {
     // remove following line if you want to show the banner after installation
-    showInstallPrompt.value = false;
+    showIosInstallPrompt.value = false;
     alert(
       'To install this app on device:\n\n1. Tap the "Share" button (usually located at the bottom of the screen).\n\n2. Scroll down and find the "Add to Home Screen" option.\n\n3. Tap "Add" in the upper-right corner.\n\nNow, you can access the app from your home screen!'
     );
   };
+
+  const promptAndroidInstall = () => {
+    if (deferredPrompt) {
+      deferredPrompt.prompt();
+      deferredPrompt.userChoice.then((choiceResult: any) => {
+        if (choiceResult.outcome === "accepted") {
+          console.log("User accepted the A2HS prompt");
+        } else {
+          console.log("User dismissed the A2HS prompt");
+        }
+        deferredPrompt = null;
+        showAndroidInstallPrompt.value = false;
+      });
+    }
+  };
+
+  onMounted(() => {
+    if (isInStandaloneMode()) {
+      showIosInstallPrompt.value = false;
+      showAndroidInstallPrompt.value = false;
+    } else {
+      if (isIos()) {
+        showIosInstallPrompt.value = true;
+      }
+
+      window.addEventListener("beforeinstallprompt", (e: Event) => {
+        e.preventDefault();
+        deferredPrompt = e;
+        if (isAndroid()) {
+          showAndroidInstallPrompt.value = true;
+        }
+      });
+    }
+
+    window.addEventListener("appinstalled", () => {
+      console.log("PWA was installed");
+      showIosInstallPrompt.value = false;
+      showAndroidInstallPrompt.value = false;
+    });
+  });
 </script>
 
 <style>
-  input {
-    background-clip: text !important;
-    -webkit-background-clip: text !important;
-  }
   .install-banner {
     position: fixed;
     bottom: 16px;
