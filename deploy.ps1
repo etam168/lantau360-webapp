3f116f5355f78021a01693d755f455bde41a1e95
Eddie Tam <etam168@gmail.com>
2024-05-30 21:10:01 +0800

    amend deploy.ps1

--- deploy.ps1
+++ deploy.ps1
@@ -1,14 +1,46 @@
-# Navigate to the dist folder
-cd .\dist
+param (
+    [string]$BuildMode = "staging"
+)
 
-# Compress the contents inside the dist folder into a zip file
-Compress-Archive -Path .\* -DestinationPath "..\dist.zip"
+# Define the credentials
+$remoteUsername = "root"
+$remoteHost = "97.74.95.249"
+$stagingPath = "/home/host1/domains/app-dev.lantau360.com/public_html"
+$productionPath = "/home/host1/domains/app.lantau360.com/public_html"
 
-# Navigate back to the parent directory
-cd ..
+# Set the build command and destination path based on the build mode
+if ($BuildMode -eq "production") {
+    $BuildCommand = "yarn build"
+    $remotePath = $productionPath
+}
+else {
+    $BuildCommand = "yarn build --mode staging"
+    $remotePath = $stagingPath
+}
 
-# Copy the zip file to the server using SCP
-scp .\dist.zip root@97.74.95.249:/home/host1/domains/app.lantau360.com/public_html/
+$DistFolder = "dist"
 
-# Remove the local zip file
-Remove-Item .\dist.zip
+# Run the build command
+Write-Host "Running build command: $BuildCommand"
+Invoke-Expression $BuildCommand
+
+# Check if the build was successful
+if ($LASTEXITCODE -eq 0) {
+    Write-Host "Build completed successfully."
+    
+    # Use scp to upload files from the dist folder to the remote server
+    $SCPCommand = "scp -r -C ${DistFolder}/* ${remoteUsername}@${remoteHost}:${remotePath}"
+    Write-Host "Running SCP command: $SCPCommand"
+    Invoke-Expression $SCPCommand
+    
+    # Check if the SCP command was successful
+    if ($LASTEXITCODE -eq 0) {
+        Write-Host "Files uploaded successfully to ${remoteUsername}@${remoteHost}:${remotePath}"
+    }
+    else {
+        Write-Host "Failed to upload files. SCP command exited with code: $LASTEXITCODE"
+    }
+}
+else {
+    Write-Host "Build failed. Exiting script."
+}
