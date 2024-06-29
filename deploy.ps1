91202cf9f6536a7651e623f879eb7c493931ce5b
Suleman Khalid <sulemankhalid218@gmail.com>
2024-06-29 12:14:50 +0500

    Fix up deploy.ps1

--- deploy.ps1
+++ deploy.ps1
@@ -3,11 +3,11 @@ param (
 )
 
 # Define the credentials
-$remoteUsername = "root"
+$remoteUsername = "lantau360"
 $remoteHost = "97.74.95.249"
 
 # Define the home folder and paths
-$homeFolder = "/home/host1"
+$homeFolder = "/home/lantau360"
 $domainName = "lantau360.com"
 $stagingPath = "${homeFolder}/domains/app-dev.${domainName}/public_html"
 $productionPath = "${homeFolder}/domains/app.${domainName}/public_html"
