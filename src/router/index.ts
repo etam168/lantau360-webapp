ec23e61f539f77ea48f0691c897434cb30cd68ca
suleman <sulemankhalid218@gmail.com>
2024-10-22 09:10:45 +0500

    Remove extra routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,12 +1,11 @@
 import { createRouter, createWebHistory } from "vue-router";
 
 import drawerRouter from "./modules/drawer";
-import extraRoutes from "./modules/extra";
 export const asyncRoutes = [...drawerRouter];
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [...asyncRoutes, ...extraRoutes]
+  routes: [...asyncRoutes]
 });
 
 export default router;
