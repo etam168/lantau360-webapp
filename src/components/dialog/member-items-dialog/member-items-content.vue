cedd1eedaaa332ca4e41ca938dfb842aca1131b5
suleman <suleman.khalid@ssidecisions.com>
2024-11-01 09:10:51 +0500

    WIP - Checkin Dialog more page

--- src/components/dialog/member-items-dialog/member-items-content.vue
+++ src/components/dialog/member-items-dialog/member-items-content.vue
@@ -1,34 +1,7 @@
 <!-- member-items-content.vue -->
 <template>
   <q-card>
-    <member-points @top-up-points="handleTopUpPoints" />
-
-    <template v-if="groupBykey">
-      <app-tab-select :tab-items="tabItems" :current-tab="tab" @update:currentTab="setTab" />
-
-      <q-tab-panels v-model="tab">
-        <q-tab-panel
-          v-for="(item, index) in tabItems"
-          :key="index"
-          :name="item.name"
-          class="q-pa-none"
-        >
-          <template v-if="filterGroupedArray(item.name).length > 0">
-            <app-member-list-items
-              :memberItems="filterGroupedArray(item.name)"
-              :entityKey="entityKey"
-              @on-member-detail="handleDetail"
-            />
-          </template>
-
-          <template v-else>
-            <div class="text-center q-pa-md">No data</div>
-          </template>
-        </q-tab-panel>
-      </q-tab-panels>
-    </template>
-
-    <app-member-list-items v-else :memberItems :entityKey @on-member-detail="handleDetail" />
+    <app-member-list-items :memberItems :entityKey @on-member-detail="handleDetail" />
   </q-card>
 </template>
 
@@ -36,98 +9,25 @@
   // Interface
   import type { CategoryTypes } from "@/interfaces/types/category-types";
   import type { Member } from "@/interfaces/models/entities/member";
-  import type { TabItem } from "@/interfaces/tab-item";
-  import type { Transaction } from "@/interfaces/models/entities/transaction";
-  import { useUserStore } from "@/stores/user";
 
   // Constants
   import { ENTITY_URL, EntityURLKey } from "@/constants";
 
   // Props
-  const { member, entityKey, points } = defineProps<{
+  const { member, entityKey } = defineProps<{
     member: Member;
     entityKey: EntityURLKey;
     points?: Record<string, any>;
   }>();
 
-  const userStore = useUserStore();
   const { fetchData } = useApi();
   const { openCategoryDetailDialog } = useCategoryDialogService(entityKey);
 
   const memberItems = ref<CategoryTypes[]>([]);
-  const historyItems = ref<Transaction[]>([]);
-  const recentItems = ref<Transaction[]>([]);
-
-  const $q = useQuasar();
-  const entityData = ref<Record<string, any>>({});
-
-  const groupBykey = computed<string | null>(() => {
-    switch (entityKey) {
-      case "TRANSACTION":
-        return "transaction";
-      // Add other cases here if needed
-      default:
-        return null;
-    }
-  });
-
-  // Define an interface for the group object
-  interface GroupedItems {
-    group: string | number;
-    items: CategoryTypes[];
-  }
-
-  const groupedArray = computed<GroupedItems[]>(() => {
-    switch (groupBykey.value) {
-      case "transaction":
-        return [
-          { group: "recent", items: recentItems.value },
-          { group: "history", items: historyItems.value }
-        ];
-
-      default:
-        return [];
-    }
-  });
-
-  // New computed property for tabItems
-  const tabItems = computed(() => {
-    if (entityKey === "TRANSACTION") {
-      return [
-        { name: "recent", label: "Recent" },
-        { name: "history", label: "History" }
-      ];
-    }
-
-    return groupedArray.value.map(group => ({
-      name: group.group,
-      label: group.group
-    })) as TabItem[];
-  });
-
-  const tab = ref("");
-  const setTab = (val: string) => (tab.value = val);
-
-  function filterGroupedArray(groupName: string) {
-    const items = groupedArray.value.find(group => group.group === groupName)?.items || [];
-    return items.sort((a: any, b: any) => a.rank - b.rank);
-  }
 
   async function handleDetail(item: any) {
-    openCategoryDetailDialog(item);
+    openCategoryDetailDialog(item,"CheckInItemDetailDialog");
   }
-
-  function handleTopUpPoints() {
-    $q.dialog({
-      component: defineAsyncComponent(
-        () => import("@/components/dialog/top-up-detail-dialog/index.vue")
-      ),
-      componentProps: { entityKey: entityKey }
-    });
-  }
-
-  const addField = (transactions: any[], type: string) =>
-    transactions.map(item => ({ ...item, transactionType: type }));
 
   const fetchAllData = async () => {
     try {
@@ -135,42 +35,8 @@
         case "CHECKIN":
           memberItems.value = await fetchData(`${ENTITY_URL.CHECKIN_BY_MEMBER}/${member.memberId}`);
           break;
-
-        case "TRANSACTION":
-          // Fetch data from two different APIs concurrently
-          const [history, recent, mem, memberPoints, memberConfig] = await Promise.all([
-            fetchData(`${ENTITY_URL.MEMBER_TRANSACTIONS}/${member.memberId}`),
-            fetchData(`${ENTITY_URL.MEMBER_RECENT_TRANSACTIONS}/${member.memberId}`),
-            fetchData(`${ENTITY_URL.MEMBER_BY_ID}/${member.memberId}`),
-            fetchData(`${ENTITY_URL.MEMBER_POINTS}/${member.memberId}`),
-            fetchData(`${ENTITY_URL.MEMBER_CONFIG}`)
-          ]);
-
-          historyItems.value = history;
-          recentItems.value = recent;
-          memberItems.value = [...recent, ...history];
-
-          entityData.value.history = history;
-          entityData.value.recent = recent;
-          entityData.value.member = mem;
-
-          const { total, spend, available, currentMonthTransactionCount } = memberPoints;
-          userStore.setPointsInfo({ total, spend, available, currentMonthTransactionCount });
-
-          userStore.setPoints(
-            memberConfig.value?.meta.postPoint ?? 50,
-            memberConfig.value?.meta.requestFreePoints ?? 100,
-            memberConfig.value?.meta.purchsePrice ?? 100,
-            memberConfig.value?.meta.purchsePoints ?? 100
-          );
-
         default:
           console.warn(`Unsupported entity type: ${entityKey}`);
-      }
-
-      // Set the initial tab value after data is fetched
-      if (memberItems.value.length > 0 && groupBykey.value) {
-        tab.value = tabItems.value.length > 0 ? tabItems.value[0].name : "";
       }
     } catch (error) {
       console.error("Error fetching data:", error);
--- src/components/global/custom/app-member-list-items.vue
+++ src/components/global/custom/app-member-list-items.vue
@@ -1,24 +1,44 @@
 <template>
-  <q-list>
-    <q-item clickable @click="handleDetail(item)" v-for="(item, index) in memberItems" :key="index">
-      <q-item-section avatar>
-        <q-avatar size="64px" circle>
-          <q-img ratio="1" :src="getImageURL((item as PostingView).memberImage)">
-            <template v-slot:error>
-              <div class="absolute-full flex flex-center bg-negative text-white">
-                {{ $t("errors.cannotLoadImage") }}
-              </div>
-            </template>
-          </q-img>
-        </q-avatar>
-      </q-item-section>
+  <q-card v-for="(item, index) in memberItems" :key="index" class="q-ma-md">
+    <q-card-section class="q-pa-none">
+      <q-list>
+        <q-item clickable @click="handleDetail(item)">
+          <q-item-section avatar>
+            <q-avatar size="64px" circle>
+              <q-img ratio="1" :src="computeIconPath(item)">
+                <template v-slot:error>
+                  <div class="absolute-full flex flex-center bg-negative text-white">
+                    {{ $t("errors.cannotLoadImage") }}
+                  </div>
+                </template>
+              </q-img>
+            </q-avatar>
+          </q-item-section>
 
-      <q-item-section>
-        <q-item-label> {{ line1(item) }} </q-item-label>
-        <q-item-label> {{ line2(item) }} </q-item-label>
-      </q-item-section>
-    </q-item>
-  </q-list>
+          <q-item-section>
+            <!-- <q-item-label> {{ line1(item) }} </q-item-label> -->
+            <q-item-label>{{ item.siteName }}</q-item-label>
+            <q-item-label lines="2"
+              >{{
+                $t("more.checkin.lastCheckIn", {
+                  date: dateFormatter(item.modifiedAt)
+                })
+              }}
+            </q-item-label>
+            <!-- <q-item-label>{{ item.siteName }}</q-item-label> -->
+
+            <!-- <q-item-label lines="2"
+          >{{
+            $t("more.checkIn.lastCheckIn", {
+              date: dateFormatter(item.modifiedAt)
+            })
+          }}
+        </q-item-label> -->
+          </q-item-section>
+        </q-item>
+      </q-list>
+    </q-card-section>
+  </q-card>
 </template>
 
 <script setup lang="ts">
@@ -26,10 +46,10 @@
   import { CategoryTypes } from "@/interfaces/types/category-types";
 
   // .ts files
-  import { EntityURLKey } from "@/constants";
+  import { BLOB_URL, EntityURLKey } from "@/constants";
   import { PostingView } from "@/interfaces/models/views/posting-view";
 
-  const { getEntityName, getImageURL, translate } = useUtilities();
+  const { getEntityName, getImageURL, translate, dateFormatter } = useUtilities();
 
   const emits = defineEmits(["on-member-detail"]);
 
@@ -56,10 +76,9 @@
     return translate(item[name] as string, item.meta, name);
   }
 
-  function line2(item: CategoryTypes) {
-    return translate(item.subtitle1, item.meta, "subtitle1");
-  }
-
+  const computeIconPath = (item: any) => {
+    return item.iconPath ? `${BLOB_URL}/${item.iconPath}` : "./img/icons/no_image_available.jpeg";
+  };
   function handleDetail(item: any) {
     emits("on-member-detail", item);
   }
