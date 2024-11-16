4126f3d7bff412184ba767d358ccd3834cefb906
hussain <hussain.mansoor@landisgyr.com>
2024-11-16 13:31:52 +0500

    Completed checkin flow

--- src/composable/services/use-checkin-data-service.ts
+++ src/composable/services/use-checkin-data-service.ts
@@ -1,53 +1,60 @@
 import type { CategoryTypes } from "@/interfaces/types/category-types";
+import type { Content } from "@/interfaces/models/entities/content";
+import type { CheckIn } from "@/interfaces/models/entities/checkin";
 import { useGeolocation } from "@vueuse/core";
 import L from "leaflet";
-import { EntityURLKey } from "@/constants";
+import { ENTITY_URL } from "@/constants";
 import { useUserStore } from "@/stores/user";
+import { SiteView } from "@/interfaces/models/views/site-view";
 
 export function useCheckInDataService() {
-  // Reactive variables
-  const memberConfig = ref();
-  const checkInData = ref();
 
+  const $q = useQuasar();
   const userStore = useUserStore();
-
+  const { handleOpenDialog } = useEntityDataHandlingService();
+  const { fetchData } = useApi();
   const { notify } = useUtilities();
   const distanceToDestination = ref(0);
   const timeUntilNextCheckIn = ref(0);
   const { coords: userLocation } = useGeolocation();
 
-  const performCheckIn = async (entityKey: EntityURLKey, category: CategoryTypes) => {
-    await checkRecentCheckInStatus();
+  function openCheckInDialog(category: CategoryTypes) {
+    const isDialogOpen = ref(false);
+    const props = { associatedEntityId: (category as SiteView).siteId, entityKey: "CHECKIN" };
+    handleOpenDialog(props, isDialogOpen, "CHECKIN");
+  }
 
-    if (timeUntilNextCheckIn.value > 0) {
-      notify(
-        `You must wait ${timeUntilNextCheckIn.value} minutes before checking in again.`,
-        "primary"
-      );
-      return; // Exit if the user can't check in yet
-    }
+  function promptUserLogon() {
+    $q.dialog({
+      component: defineAsyncComponent(() => import("@/components/dialog/login-alert-dialog.vue")),
+      componentProps: {
+        mode: "login"
+      }
+    });
+  }
 
-    await getDistanceToDestination(category);
-    if (distanceToDestination.value > 100) {
-      notify("You must be under 100 meters of location for check-in", "primary");
-    } else {
-      handleCheckIn(entityKey, category);
-    }
-  };
-
-  const getDistanceToDestination = async (category: CategoryTypes) => {
-    const { latitude: siteLatitude, longitude: siteLongitude } = category;
+  function isOutOfRange(category: CategoryTypes) {
     const sourcePoint = L.latLng(userLocation.value.latitude, userLocation.value.longitude);
-    const destinationPoint = L.latLng(siteLatitude, siteLongitude);
+    const destinationPoint = L.latLng(category.latitude, category.longitude);
 
     // Returns distance in meters
-    distanceToDestination.value = sourcePoint.distanceTo(destinationPoint);
-  };
+    const distance = sourcePoint.distanceTo(destinationPoint);
 
-  const checkRecentCheckInStatus = async () => {
+    // Returns true if distance is greater than 10 meters
+    return distance > 10;
+  }
+
+  async function hasLast24HrsCheckIn(category: CategoryTypes) {
     try {
-      const config = memberConfig.value;
-      const checkIn = checkInData.value;
+      const [memberConfigRes, checkInDataRes] = await Promise.all([
+        fetchData<Content>(ENTITY_URL.MEMBER_CONFIG),
+        fetchData<CheckIn>(
+          `${ENTITY_URL.MEMBER_SITE_CHECK_IN}/${userStore.userId}/${(category as SiteView).siteId}`
+        )
+      ]);
+
+      const config = memberConfigRes;
+      const checkIn = checkInDataRes;
 
       const configTimeDifferenceInHours = config?.meta?.checkInTimeDifferenceInHours ?? 1;
       const configTimeDifferenceInMinutes = configTimeDifferenceInHours * 60;
@@ -62,51 +69,18 @@ export function useCheckInDataService() {
 
       if (minutesLeftToRecheckIn >= 0) {
         timeUntilNextCheckIn.value = Math.ceil(minutesLeftToRecheckIn); // Round up to nearest minute
+        return true;
       } else {
         timeUntilNextCheckIn.value = 0; // User can check in now
+        return false;
       }
-    } catch (err) {
-      console.error("Error checking recent check-in status:", err);
+    } catch (error) {
+      console.error("Error fetching member check-in details:", error);
+      throw error;
     }
-  };
-
-  const handleCheckIn = (entityKey: EntityURLKey, category: CategoryTypes) => {
-    const props = { associatedEntityId: category.siteId, entityKey: entityKey };
-    // Assuming you have a dialog handling service
-    openCheckInDialog(props);
-  };
-
-  const openCheckInDialog = props => {
-    alert("openCheckInDialog");
-    // Implement your dialog opening logic here
-    // Example: Use a dialog service to open the check-in dialog with the props
-  };
-
-  function promptUserLogon() {
-    // $q.dialog({
-    //   component: defineAsyncComponent(() => import("@/components/dialog/login-alert-dialog.vue")),
-    //   componentProps: {
-    //     mode: "login"
-    //   }
-    // });
   }
 
-  function isOutOfRange(category: CategoryTypes) {
-    const sourcePoint = L.latLng(userLocation.value.latitude, userLocation.value.longitude);
-    const destinationPoint = L.latLng(category.latitude, category.longitude);
-
-    // Returns distance in meters
-    const distance = sourcePoint.distanceTo(destinationPoint);
-
-    // Returns true if distance is greater than 10 meters
-    return distance > 10;
-  }
-
-  function hasLast24HrsCheckIn(category: CategoryTypes) {
-    return false;
-  }
-
-  function requestCheckIn(category: CategoryTypes) {
+  async function requestCheckIn(category: CategoryTypes) {
     try {
       switch (true) {
         case userStore.isUserLogon() == false:
@@ -115,9 +89,15 @@ export function useCheckInDataService() {
         case isOutOfRange(category):
           notify("You must be under 100 meters of location for check-in", "primary");
           return;
-        case hasLast24HrsCheckIn(category):
-          notify("You must be under 100 meters of location for check-in", "primary");
+        case await hasLast24HrsCheckIn(category):
+          notify(
+            `You must wait ${timeUntilNextCheckIn.value} minutes before checking in again.`,
+            "primary"
+          );
           return;
+        default:
+          openCheckInDialog(category);
+          break;
       }
     } catch {
       // kk
