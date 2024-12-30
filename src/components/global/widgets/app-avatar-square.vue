1c456805f4948055866b9de5172489673cb39f2a
Eddie Tam <etam168@gmail.com>
2024-12-30 10:37:46 +0800

    add new component app-avatar-square

---
+++ src/components/global/widgets/app-avatar-square.vue
@@ -0,0 +1,30 @@
+<template>
+  <q-avatar size="64px" square class="bg-grey-2">
+    <q-img
+      :src="getImageURL(imagePath)"
+      spinner-color="primary"
+      spinner-size="24px"
+      fit="cover"
+      loading="lazy"
+      @error="onImageError"
+    >
+      <template v-slot:error>
+        <q-img :src="IMAGES.NO_IMAGE_AVAILABLE_PLACEHOLDER" fit="cover" />
+      </template>
+    </q-img>
+  </q-avatar>
+</template>
+
+<script setup lang="ts">
+  import { IMAGES } from "@/constants";
+
+  const { imagePath } = defineProps<{
+    imagePath: string;
+  }>();
+
+  const { getImageURL } = useUtilities();
+
+  const onImageError = () => {
+    console.warn(`Failed to load image: ${imagePath}`);
+  };
+</script>
