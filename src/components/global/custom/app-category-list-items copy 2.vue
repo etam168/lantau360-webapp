ed32ab2837c375b886ecf115490eaa0b59dea5c3
Eddie Tam <etam168@gmail.com>
2024-12-31 13:52:24 +0800

    sample code for Shabeena to follow

---
+++ src/components/global/custom/app-category-list-items copy 2.vue
@@ -0,0 +1,101 @@
+<template>
+  <q-list v-if="categoryItems.length > 0" v-for="(item, index) in categoryItems" :key="index">
+    <q-item clickable @click="handleDetail(item)">
+      <q-item-section avatar>
+        <app-avatar-rounded :image-path="item.iconPath" />
+      </q-item-section>
+
+      <q-item-section>
+        <q-item-label>{{ line1(item) }}</q-item-label>
+        <q-item-label caption>{{ line2(item) }}</q-item-label>
+      </q-item-section>
+
+      <q-item-section side>
+        <div class="q-gutter-sm">
+          <q-icon :name="fasLocationDot" size="xs" v-if="isCheckedIn(item)" />
+          <q-icon :name="fasHeart" color="red" size="xs" v-if="isFavoriteItem(item)" />
+        </div>
+      </q-item-section>
+    </q-item>
+
+    <q-separator />
+  </q-list>
+
+  <app-no-record-message v-else :message="$t('errors.noRecord')" />
+</template>
+
+<script setup lang="ts">
+  // Interface files
+  import type { CategoryTypes } from "@/interfaces/types/category-types";
+  import type { CheckIn } from "@/interfaces/models/entities/checkin";
+  import type { DirectoryTypes } from "@/interfaces/types/directory-types";
+  import type { SiteView } from "@/interfaces/models/views/site-view";
+
+  // Constants
+  import { EntityURLKey } from "@/constants";
+  import { fasHeart, fasLocationDot } from "@quasar/extras/fontawesome-v6";
+
+  // Stores
+  import { useFavoriteStore } from "@/stores/favorite-store";
+
+  // Emits
+  const emits = defineEmits(["on-category-detail"]);
+
+  // Props
+  const {
+    categoryItems,
+    checkIns = [],
+    entityKey,
+    directory
+  } = defineProps<{
+    categoryItems: CategoryTypes[];
+    checkIns?: CheckIn[];
+    entityKey: EntityURLKey;
+    directory?: DirectoryTypes;
+  }>();
+
+  const { locale } = useI18n({ useScope: "global" });
+  const { getEntityName, translate } = useUtilities(locale.value);
+
+  const entityName = getEntityName(entityKey);
+  const favoriteStore = useFavoriteStore();
+
+  function line1(item: CategoryTypes): string | null {
+    if (directory && directory.groupId === 5) {
+      return null;
+    }
+    const name = `${entityName}Name` as keyof CategoryTypes;
+    return translate(item[name] as string, item.meta, name);
+  }
+
+  function line2(item: CategoryTypes): string {
+    if (directory && directory.groupId === 5) {
+      return title(item);
+    }
+    if (directory?.meta.template === 3) {
+      return title(item);
+    }
+    return translate(item.subtitle1, item.meta, "subtitle1");
+  }
+
+  function title(item: CategoryTypes) {
+    return translate(item.title, item.meta, "title");
+  }
+
+  const isCheckedIn = (item: CategoryTypes): boolean => {
+    if (entityKey === "SITE") {
+      return checkIns.some(
+        checkInItem => (checkInItem as CheckIn).siteId === (item as SiteView).siteId
+      );
+    }
+    return false;
+  };
+
+  const isFavoriteItem = (item: CategoryTypes): boolean => {
+    return favoriteStore.isFavorite(item as any, entityKey);
+  };
+
+  function handleDetail(item: any) {
+    emits("on-category-detail", item);
+  }
+</script>
