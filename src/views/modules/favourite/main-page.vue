c2d96b760dc673fe6bc2228f80ebb15677c00aca
suleman <sulemankhalid218@gmail.com>
2025-01-05 12:35:14 +0500

    Merge main-content into main-page for  fav module

--- src/views/modules/favourite/main-page.vue
+++ src/views/modules/favourite/main-page.vue
@@ -4,25 +4,61 @@
 
   <q-separator size="4px" color="primary" />
 
-  <main-content :i18n-key="i18nKey" @on-category-detail="onCategoryDetail" />
+  <!-- Merged main-content template -->
+  <q-banner :inline-actions="!isSmallScreen">
+    <q-toolbar-title :class="titleClass">{{ contentTitle }}</q-toolbar-title>
+
+    <template v-slot:action>
+      <app-tab-select
+        :tab-items="tabItems"
+        :current-tab="tab"
+        @update:currentTab="setTab"
+        :class="$q.screen.lt.sm ? 'justify-center' : ''"
+      />
+    </template>
+  </q-banner>
+
+  <q-tab-panels v-model="tab">
+    <q-tab-panel name="location" class="q-pa-none">
+      <app-category-list-items
+        :categoryItems="siteItems"
+        :entityKey="'SITE'"
+        :pageName="'FAVOURITE'"
+        @on-category-detail="onCategoryDetail"
+      />
+    </q-tab-panel>
+
+    <q-tab-panel name="business" class="q-pa-none">
+      <app-category-list-items
+        :categoryItems="businessItems"
+        :entityKey="'BUSINESS'"
+        :pageName="'FAVOURITE'"
+        @on-category-detail="onCategoryDetail"
+      />
+    </q-tab-panel>
+
+    <q-tab-panel name="checkIn" class="q-pa-none">
+      <app-checkin-list-items :style="tableStyle" i18n-key="favourite.checkIn" />
+    </q-tab-panel>
+  </q-tab-panels>
 </template>
 
 <script setup lang="ts">
   // Interface files
   import type { AdvertisementView } from "@/interfaces/models/views/advertisement-view";
-
-  // Custom Components
-  const mainContent = defineAsyncComponent(() => import("./components/main-content.vue"));
+  import type { CheckInView } from "@/interfaces/models/views/checkin-view";
+  import type { TabItem } from "@/interfaces/tab-item";
+  import type { SiteView } from "@/interfaces/models/views/site-view";
+  import type { BusinessView } from "@/interfaces/models/views/business-view";
 
   // Constants
-  import { EntityURLKey, ENTITY_URL } from "@/constants";
+  import { EntityURLKey, ENTITY_URL, MAX_SCREEN_WIDTH } from "@/constants";
 
   // Composables
   import { useUserStore } from "@/stores/user";
   import { useFavoriteStore } from "@/stores/favorite-store";
   import { useCheckInStore } from "@/stores/checkin-store";
 
-  // Props
   const { entityKey } = defineProps<{
     entityKey: EntityURLKey;
   }>();
@@ -35,19 +71,51 @@
   const { t } = useI18n({ useScope: "global" });
   const { fetchData } = useApi();
   const { openCategoryDetailDialog } = useCategoryDialogService(entityKey);
-  const { eventBus, getEntityName } = useUtilities();
+  const { eventBus, getEntityName, isSmallScreen, aspectRatio } = useUtilities();
 
   const advertisements = ref<any | null>(null);
   const error = ref<string | null>(null);
-
   const i18nKey = getEntityName(entityKey);
   const isDialogOpen = ref(false);
+
+  const siteItems = computed<SiteView[]>(() => favStore.favoriteSites);
+  const businessItems = computed<BusinessView[]>(() => favStore.favoriteBusinesses);
+  const checkinItems = computed<CheckInView[]>(() => checkInStore.checkInSites);
+
+  const titleClass = computed(() => (isSmallScreen.value ? "text-center" : ""));
+  const tab = ref("location");
+
+  const tabItems = ref<TabItem[]>([
+    { name: "location", label: t(`${i18nKey}.tabItem.location`) },
+    { name: "business", label: t(`${i18nKey}.tabItem.business`) },
+    { name: "checkIn", label: t(`${i18nKey}.tabItem.checkIn`) }
+  ]);
+
+  const setTab = (val: string) => (tab.value = val);
+
+  const contentTitle = computed(() => {
+    const baseTitle = t(`${i18nKey}.title`);
+    return userStore.isUserLogon() ? baseTitle : `${baseTitle} (Offline)`;
+  });
+
+  const THRESHOLD = 320 as const;
+
+  const usedHeight = computed(() => {
+    const ADDITIONAL_HEIGHT = 160 as const;
+    const width = Math.min($q.screen.width, MAX_SCREEN_WIDTH);
+    const carouselHeigh = width * aspectRatio();
+
+    return carouselHeigh + ADDITIONAL_HEIGHT;
+  });
+
+  const tableStyle = computed<Record<string, any> | undefined>(() => {
+    const height = $q.screen.height - usedHeight.value;
+    return height > THRESHOLD ? { height: `calc(100vh - ${usedHeight.value}px)` } : undefined;
+  });
 
   async function fetchAllData() {
     try {
       const [advertisementResponse] = await Promise.all([fetchData(ENTITY_URL.ADVERTISEMENT)]);
-      advertisements.value = advertisementResponse;
-
       advertisements.value = advertisementResponse.filter(
         (adv: AdvertisementView) => adv.status === 1
       );
@@ -75,7 +143,6 @@
     $q.dialog({
       component: defineAsyncComponent(() => import("./components/data-sync-alert-dialog.vue"))
     }).onOk(async (selectedOption: string) => {
-      // Reset dialog state when it is dismissed/closed
       isDialogOpen.value = false;
 
       switch (selectedOption) {
@@ -110,9 +177,5 @@
     await openCategoryDetailDialog(item, dialogName, entityKey);
   }
 
-  /**
-   * Fetch data as part of the setup
-   * This ensures that the component is compatible with Suspense
-   */
   await fetchAllData();
 </script>
