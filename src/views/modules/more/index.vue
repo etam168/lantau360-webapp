af0f187543989634940acbf5708f6106ba493b84
suleman <sulemankhalid218@gmail.com>
2025-01-25 14:35:40 +0500

    Fix more page back press button issue

--- src/views/modules/more/index.vue
+++ src/views/modules/more/index.vue
@@ -10,5 +10,36 @@
   // Custom Components
   import MainContent from "./components/main-content.vue";
 
+  // Stores
+  import { useOpenDialogStore } from "@/stores/open-dialog-store";
+  import { useUserStore } from "@/stores/user";
+  import { EventBus } from "quasar";
+
+  const bus = inject("bus") as EventBus;
+
   const i18nKey = "more";
+
+  const openDialogStore = useOpenDialogStore();
+  const userLogon = UserLogon();
+  const userStore = useUserStore();
+
+  onBeforeRouteLeave((_to, _from, next) => {
+    switch (true) {
+      case openDialogStore.hasDialogId(): {
+        const dialogId = openDialogStore.getLatestDialogId();
+        bus.emit("DialogClose", dialogId);
+        next(false); // Prevent navigation if dialogId exists
+        break;
+      }
+      case _to.name === "favourite" && !userStore.isUserLogon(): {
+        userLogon.localDataNotification();
+        next();
+        break;
+      }
+      default: {
+        next();
+        break;
+      }
+    }
+  });
 </script>
