58dd144ab4dd3b404f3d6676cb479a9191c95ca0
suleman <sulemankhalid218@gmail.com>
2025-01-27 08:32:56 +0500

    WIP - install-item-content

--- src/components/dialog/install-items-dialog/install-items-content.vue
+++ src/components/dialog/install-items-dialog/install-items-content.vue
@@ -1,6 +1,5 @@
 <template>
-  <template v-for="(item, index) in renderItems" :key="index">
-    <q-card v-if="item.type === 'edge'">
+    <q-card v-if="browserType !== 'unknow'">
       <q-toolbar class="bg-primary text-white">
         <q-toolbar-title class="text-weight-bold">
           {{ $t("notification.installApp") }}
@@ -11,20 +10,15 @@
 
       <q-card-section>
         <div>
-          {{ "Follow the instructions to install the app on Edge." }}
+          {{ "Follow the instructions to install the app" }}
         </div>
       </q-card-section>
 
       <q-card-section>
-        <img :src="PLATFORM.FIREFOX" alt="Install Image" />
+        <img :src="platformIcon" alt="Install Image" />
       </q-card-section>
     </q-card>
-
-    <!-- <install-edge-section v-if="item.type === 'edge'" /> -->
-    <install-ios-section v-else-if="item.type === 'ios'" />
-    <install-opera-section v-else-if="item.type === 'opera'" />
     <install-complete-section v-else />
-  </template>
 </template>
 
 <script setup lang="ts">
@@ -35,26 +29,24 @@
   import { fasXmark } from "@quasar/extras/fontawesome-v6";
 
   import InstallCompleteSection from "./sections/install-complete-section.vue";
-  // import InstallEdgeSection from "./sections/install-edge-section.vue";
-  import InstallIosSection from "./sections/install-ios-section.vue";
-  import InstallOperaSection from "./sections/install-opera-section.vue";
 
-  interface RenderItem {
-    name: string;
-    type: "edge" | "ios" | "opera";
-  }
+  const browserType = computed(() => {
+    if (Platform.is.edge) return "edge";
+    if (Platform.is.ios) return "ios";
+    if (Platform.is.opera) return "opera";
+    return "unknow";
+  });
 
-  const renderItems = computed((): RenderItem[] => {
-    switch (true) {
-      case Platform.is.edge:
-        return [{ name: "edge", type: "edge" }];
-      case Platform.is.ios:
-        return [{ name: "ios", type: "ios" }];
-      case Platform.is.opera:
-        return [{ name: "opera", type: "opera" }];
+  const platformIcon = computed(() => {
+    switch (browserType.value) {
+      case "edge":
+        return PLATFORM.FIREFOX; //To do need edge resource
+      case "ios":
+        return PLATFORM.IOS;
+      case "opera":
+        return PLATFORM.OPERA;
       default:
-        return [{ name: "opera", type: "opera" }];
-      // To be implemented: Handle unknown browsers with a generic message or action
+        return PLATFORM.FIREFOX; // Default fallback icon
     }
   });
 </script>
